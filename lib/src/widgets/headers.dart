f0999cbb7b7e27f053e9786f90ba54cd1f2476b1
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-27 16:46:25 -0500

    HeaderWave

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -187,3 +187,46 @@ class _HeaderCurvoPainter extends CustomPainter {
     return true;
   }
 }
+
+
+class HeaderWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; //.fill rellenar
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    //Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.30); 
+    path.quadraticBezierTo(size.width*0.25, size.height*0.35, size.width*0.5, size.height*0.30);
+    path.quadraticBezierTo(size.width*0.75, size.height*0.25, size.width, size.height*0.30);
+    path.lineTo(size.width, 0);
+    // path.lineTo(size.width, size.height*0.25);
+
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
