3a0dceb51d275ce35aa46ee8e5d49401d6b1f618
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-27 17:48:17 -0500

    HeaderWaveBottom

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -230,3 +230,50 @@ class _HeaderWavePainter extends CustomPainter {
     return true;
   }
 }
+
+
+class HeaderWaveBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWaveBottomPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWaveBottomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; //.fill rellenar
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    //Dibujar con el path y el lapiz
+    // path.lineTo(0, size.height * 0.30); 
+    // path.quadraticBezierTo(size.width*0.25, size.height*0.35, size.width*0.5, size.height*0.30);
+    // path.quadraticBezierTo(size.width*0.75, size.height*0.25, size.width, size.height*0.30);
+    // path.lineTo(size.width, 0);
+ 
+    path.moveTo(0, size.height * 0.80); 
+    path.quadraticBezierTo(size.width*0.25, size.height*0.75, size.width*0.5, size.height*0.80);
+    path.quadraticBezierTo(size.width*0.75, size.height*0.85, size.width, size.height*0.80);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
