40c08e59bb48ebc59c7b83049bc2c9b21adac6a8
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-27 16:18:38 -0500

    Header Diagonal

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -1,12 +1,11 @@
+import 'package:custom_painter/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
 class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: HeaderDiagonal()
     );
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -5,8 +5,64 @@ class HeaderCuadrado extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      color:Colors.black
+      color:Color(0xff615AAB),
       
     );
+  }
+}
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft:Radius.circular(70), bottomRight:Radius.circular(70) ),
+        color:Color(0xff615AAB),
+      ),   
+    );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+      
+      final lapiz = Paint();
+      
+      // Propiedades
+      lapiz.color       = Color(0xff615AAB);
+      lapiz.style       = PaintingStyle.fill; //.fill rellenar
+      lapiz.strokeWidth = 2;
+
+      final path = new Path();
+
+      //Dibujar con el path y el lapiz
+      path.moveTo(0         , size.height*0.35);
+      path.lineTo(size.width, size.height*0.30);
+      path.lineTo(size.width, 0);
+      path.lineTo(0, 0);
+      
+      // path.lineTo(0, size.height*0.5);      
+      canvas.drawPath(path, lapiz);
+
+    }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
